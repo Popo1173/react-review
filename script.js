680d74c376ae2764f209c728e5103497b3b0f030
荒井忠輔 <mr.popo1173@gmail.com>
2021-03-25 03:27:30 +0900

    jsx2-2

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 let dom = document.querySelector('#root');
 let element  =  React.createElement(
-    'p', {}, 'Hello  Ract Application'
-);
+    'div', {}, [
+        React.createElement(
+            'h2', {}, "Hellow"
+        ),
+        React.createElement(
+            'h3', {}, "React  sample page"
+        ),
+        React.createElement(
+            'ul', {}, [
+                React.createElement(
+                    'li', {}, "FirstItem"
+                ),
+                React.createElement(
+                    'li', {}, "SecondItem"
+                ),
+                React.createElement(
+                    'li', {}, "ThirdItem"
+                ),
+            ]
+        ),
+    ]);
 ReactDOM.render(element, dom);
