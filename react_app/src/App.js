e6aca5f8c88691b2899ede4ee2ee4c4a50ce6390
荒井忠輔 <mr.popo1173@gmail.com>
2021-04-08 08:54:02 +0900

    複数コンポーネント

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,38 +3,43 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super();
-    this.title = props.title;
-    this.message = props.message;
-  }
   render() {
     return  <div>
-      <h1>{this.title}</h1>
-      <p>{this.message}</p>
-    </div>
+      <h1>React</h1>
+      <Rect x="50" y="50" w="150" h="150" c="cyan" />
+      <Rect x="150" y="100" w="150" h="150" c="magenta" />
+      <Rect x="100" y="150" w="150" h="150" c="black" />
+    </div>;
   }
 }
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
+class Rect extends Component {
+  x=0;
+  y=0;
+  width=0;
+  height=0;
+  color="white";
+  style={};
+
+  constructor(props) {
+    super(props);
+    this.x = props.x;
+    this.y = props.y;
+    this.width = props.w;
+    this.height = props.h;
+    this.color = props.c;
+    this.style  = {
+      backgroundColor: this.color,
+      position: "absolute",
+      left: this.x + "px",
+      top: this.y + "px",
+      width: this.width + "px",
+      height: this.height + "px"
+    }
+  }
+  render() {
+    return <div style={this.style}></div>;
+  }
+}
 
 export default App;
