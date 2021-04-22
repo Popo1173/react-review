578b968c5775263dd23bf6bbeece4bb0a8fd0773
荒井忠輔 <mr.popo1173@gmail.com>
2021-04-22 09:25:32 +0900

    コメント作成中

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -3,64 +3,84 @@ import React, { Component } from 'react';
 import Rect from './Rect'
 import './App.css';
 
+//表示するコンポーネント
 class App extends Component {
-  //空の配列
-  data = []
-
+  //配列
+  data = [
+    "This is list sample",
+    "これはリストのサンプルです",
+    "配列をリストに変換します"
+  ];
+  //スタイル
   msgStyle = {
     fontSize: "24px",
     color: "#900",
     margin: "20px 0px",
     padding: "5px"
   }
-  area = {
-    width: "500px",
-    height: "500px",
-    border: "1px solid blue"
-  }
-
   //dataプロパティをlistステートで管理
   constructor(props){
     super(props);
     this.state = {
       list:this.data
     };
-    this.doAction = this.doAction.bind(this);
   }
-  //引数(e)からクリックした画面x,y座標を取得
-  //data[]にpushする
-  //setStateで値を更新する
-  doAction(e) {
-    let x = e.pageX
-    let y = e.pageY
-    this.data.push({x:x, y:y});
-    this.setState ({
-      list:this.data
-    });
-  }
-  //draw関数で四角形を描画する
-  draw(d){
-    let s = {
-      position: "absolute",
-      left: (d.x - 25) + "px",
-      top: (d.y - 25)  + "px",
-      width: "50px",
-      height: "50px",
-      backgroundColor: "#66f3"
-    };
-    return <div style={s}></div>
-  }
-
+  //レンダリング
   render() {
     return  <div>
       <h1>React</h1>
       <h2 style={this.msgStyle}>show rect</h2>
-      {/* /クリック位置を崇徳する/ */}
-      <div style={this.area} onClick={this.doAction}>
-        {/*data配列をmap関数に入れる　 */}
-        {this.data.map((value)=>this.draw(value))}
-      </div>
+      {/* /ListComponentにtitleを引数として渡す/ */}
+      <List title="サンプルリスト" data={this.data} />
     </div>;
+  }
+}
+
+//リスト全体をまとめて表示するComponent
+class List extends Component {
+  number = 1;
+  title = {
+    fontSize: "20px",
+    fontWeight: "bold",
+    color: "blue"
+  };
+  render() {
+    let data = this.props.data;
+    return (
+      <div>
+        <p style={this.title}>{this.props.title}</p>
+        <ul>
+          {/* map関数でdata配列を繰り返す */}
+          {data.map((item)=>
+            // ItemComponentでnumber属性に番号（nmuber=1）、valueに配列の中身（テキスト）を指定
+            <Item number={this.number++} value={item} key={this.number} />
+          )}
+        </ul>
+      </div>
+    );
+  }
+}
+
+//リストの各項目を表示するComponent
+class Item extends Component {
+  li = {
+    listStyleType: "square",
+    fontSize: "16px",
+    color: "#06",
+    margin: "0px",
+    padding: "0px"
+  }
+  num = {
+    fontWeight: "bold",
+    color: "red"
+  }
+  render() {
+    return (
+      <li style={this.li}>
+        <span style={this.num}>[{this.props.number}]</span>
+        {this.props.value}
+      </li>
+    );
   }
 }
 export default App;
