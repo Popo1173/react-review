c7feb082cb9b4f5dee2bf0be7b66ce26d6fa4fd2
荒井忠輔 <mr.popo1173@gmail.com>
2021-04-25 12:19:09 +0900

    完了

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -4,58 +4,59 @@ import Rect from './Rect'
 import './App.css';
 
 class App extends Component {
-  //inputの初期状態を空にする
   input = '';
 
-  //スタイル
   msgStyle = {
-    fontSize: "24px",
+    fontSize: "20px",
     color: "#900",
     margin: "20px 0px",
     padding: "5px"
   }
-  inputStyle = {
-    fontSize: "12px",
-    padding: "5px"
-  }
-  //stateの初期値設定
-  constructor(props){
-    super(props);
-    this.state = {
-      message: 'type your name:'
-    };
-    //doChange,doSubmit関数をbindする
-    this.doChange = this.doChange.bind(this);
-    this.doSubmit = this.doSubmit.bind(this);
-  }
-  //入力された値を取得する
-  doChange(event) {
-    this.input = event.target.value;
-  }
-  //dosubumit()発火したらsetStateで値を更新する
-  doSubmit(event) {
-    this.setState({
-      message: 'Hello. ' + this.input + '!!'
-    });
-    //発生したイベントをなくす　送信されなくなる
-    event.preventDefault();
-  }
-  //レンダリング
-  render() {
-    return  <div>
-      <h1>React</h1>
-      <h2>{this.state.message}</h2>
-      <form onSubmit={this.doSubmit}>
-        <label>
-          <spna style={this.inputStyle}></spna>
-          {/* inputを必須項目にし、「半角アルファベット、スペース、カンマ、ピリオドのみ入力化とする」 */}
-          {/* required：必須項目、pattern：正規表現のパターンを指定 */}
-          Message:<input type="type" style={this.inputStyle} onChange={this.doChange} required pattern="[A-za-z _,.]+" />
-        </label>
-        <input type="submit" style={this.inputStyle} value="click" />
-      </form>
-    </div>;
-  }
-}
 
+  constructor(props) {
+    super(props);
+    this.state ={
+      message: 'Type Your Name'
+    };
+    this.doCheck = this.doCheck.bind(this);
+  }
+
+  //
+  doCheck(event) {
+    alert(event.target.value + "は長すぎます。５文字以内にする");
+  }
+
+  render(){
+    return <div>
+      <h1>REACT</h1>
+      <h2>{this.state.message}</h2>
+      {/*  最大5文字の入力チェック */}
+      <Message maxlength="5" onCheck={this.doCheck} />
+    </div>
+    }
+  }
+
+  class Message extends Component {
+    inputStyle = {
+      fontSize: "12px",
+      padding: "5px"
+    }
+    constructor(props){
+      super(props);
+      this.doChange = this.doChange.bind(this);
+    }
+    // onChangeで値が更新されるとdoCheckが実行され、値のチェックが行われる
+    //入力された値とコンポーント親から渡された「maxlength="5"」を比較
+    //入力された値が5以上であれば、「onCheck」を実行すし、
+    doChange(e) {
+      if(e.target.value.length > this.props.maxlength) {
+        this.props.onCheck(e);
+        //入力された文字列を？？０番目から5番目までe.target.value代入する　
+        e.target.value = e.target.value.substr(0,this.props.maxlength);
+      }
+    }
+    render(){
+      return <input type="text" style={this.inputStyle} onChange={this.doChange} />
+    }
+  }
 export default App;
