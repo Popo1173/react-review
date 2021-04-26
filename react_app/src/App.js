c00f15d40b1d54240184f61f700badc1c96a79b9
荒井忠輔 <mr.popo1173@gmail.com>
2021-04-27 06:52:10 +0900

    リスト4-1,2作成

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -2,81 +2,45 @@ import React, { Component } from 'react';
 //react-redux のconnect をインポートする
 import { connect } from 'react-redux';
 import './App.css';
+import Memo from './memo/Memo';
+import AddForm from './memo/FindForm';
+import FindForm from './memo/DelForm';
 
 
-//ステートのマッピング
-//このコンポーネント利用するステートを返す関数
-function mappingState (state) {
-  return state;
-}
 
+//APPコンポーネント
 class App extends Component {
-  constructor(props){
+  td = {
+    with: "250px"
+  }
+  constructor(props) {
     super(props);
   }
-
   render(){
     return (
       <div>
-        <h1>Redux</h1>
-        <Message />
-        <Button />
+        <h1>Memo</h1>
+        <AddForm />
+        <hr />
+        <table>
+          <tbody>
+            <tr>
+              <td style={this.td}><FindForm /></td>
+              <td style={this.td}><DelForm /></td>
+            </tr>
+          </tbody>
+        </table>
+        <Memo />
       </div>
-    );
-  }
-}
-//ストアのコネクト
-App = connect()(App);
-
-//メッセージ表示コンポーネント
-class Message extends Component {
-  style= {
-    fontSize: "20px",
-    padding: "20px 5px"
-  }
-  render(){
-    return(
-      <p style={this.style}>
-        {/* ストアの値を取得 */}
-        {this.props.message}:{this.props.counter}
-      </p>
-    );
-  }
-}
-//ストアのコネクト
-//connect を呼び出し引数にmappingState
-//
-Message = connect(mappingState)(Message);
-
-//ボタン表示
-class Button extends Component {
-  style= {
-    fontSize: "16px",
-    padding: "5px 10px"
+    )
   }
 
-  constructor(props) {
-    super(props);
-    //doAActionをbind
-    this.doAction =this.doAction.bind(this)
-  }
 
-  doAction(e){
-    if(e.shiftKey) {
-      //this.props.dispatchの引数に{type: 'DECREMENT'})を指定して、レデューサーを呼び出して処理を実行
-      this.props.dispatch({type: 'DECREMENT'});
-    }else {
-      this.props.dispatch({type: 'INCREMENT'})
-    }
-  }
 
-  render(){
-    return(
-      <button style={this.style} onClick={this.doAction}>Click</button>
-    );
-  }
-}
-//ストアのコネクト
-Button = connect()(Button);
+}//APP-Component
+
+
+
+
 
 export default App;
--- react_app/src/index.js
+++ react_app/src/index.js
@@ -1,57 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//redux ストアとレデューサーをインポート
-import { createStore, combineReducers} from 'redux';
-//react-redux Providerをインポート
-import { Provider } from 'react-redux';
-import './index.css';
+import {Provider} from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import MemoStore from './memo/Store';
 
-
-//ステートの値
-//レデューサーに渡す初期値
-let state_value = {
-    counter: 0,
-    message: "COUNTER"
-}
-
-//レデューサー
-//引数に「stateとaction」を渡す
-//action「レデューサーの呼び出し内容が入ったオブジェクト（呼び出した際の情報が保管されている）」
-//state「初期値を渡す」
-function counter(state=state_value, action) {
-    //「.type」呼び出し種類のこと
-    switch(action.type){
-        case 'INCREMENT':
-            return {
-                counter: state.counter +1,
-                message: 'INCREMENT'
-            };
-        case 'DECREMENT':
-            return {
-                counter: state.counter -1,
-                message: 'DECREMENT'
-            };
-            default:
-                return state;
-    }
-}
-
-//ストアを作成
-let store = createStore(counter);
-
-//表示をレンダリング
 ReactDOM.render(
-    //store という属性にストアを指定する
-    <Provider store={store}>
+    <Provider store={MemoStore}>
         <App />
     </Provider>,
     document.getElementById('root')
-)
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+);
